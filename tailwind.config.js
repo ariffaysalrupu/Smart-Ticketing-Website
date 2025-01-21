05399dac083cd92489cfb532c80a1e76f7214906
ariffaysalrupu <arif1faysal3rupu@gmail.com>
2025-01-22 00:08:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/images/banner.png')",
+          
+        }
+      }
+    }
+  }
