036abaedae17b5e9a1e420b71895672fe5b41f7f
ariffaysalrupu <arif1faysal3rupu@gmail.com>
2025-02-25 15:35:09 +0100

    added javascript codes

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
+function scrollToSelection(){
+    document.getElementById("targetSection").scrollIntoView({ behavior: "smooth" });
+}
+
+document.getElementById("scrollButton").addEventListener("click", function);
+  
+
+const allSeatsTogether = document.getElementsByClassName("all-seats");
+
+for (const allSeats of allSeatsTogether){
+    allSeats.addEventListener("click", function (event) {
+        const ticketPrice = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[11].childNodes[3].childNodes[1].innerText;
+        const seatNumber = event.target.parentNode.childNodes[1].innerText;
+
+        // console.log(event.target.);
+    })
+}
+
+
 
 function getConvertedValue(id) {
     const price = document.getElementById(id).innerText;
@@ -6,4 +25,4 @@ function getConvertedValue(id) {
     return convertedPrice;
 }
 
-const result = getConvertedValue("")
+const result = getConvertedValue("ticket-price");
