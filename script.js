10c731a2266f219df072e1a6c859e35ae07f23c7
ariffaysalrupu <arif1faysal3rupu@gmail.com>
2025-02-25 20:16:21 +0100

    added selected seat section

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 function scrollToSelection(){
-    document.getElementById("targetSection").scrollIntoView({ behavior: "smooth" });
+    document.getElementById("target-section").scrollIntoView({ behavior: "smooth" });
 }
 
-document.getElementById("scrollButton").addEventListener("click", function);
+document.getElementById("scrollButton").addEventListener("click", scrollToSelection);
   
 
 const allSeatsTogether = document.getElementsByClassName("all-seats");
@@ -13,9 +13,36 @@ for (const allSeats of allSeatsTogether){
         const ticketPrice = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[11].childNodes[3].childNodes[1].innerText;
         const seatNumber = event.target.parentNode.childNodes[1].innerText;
 
+        const seatSelectedContainer = document.getElementById("selected-seats");
+
+        const div = document.createElement("div");
+        div.classList.add("flex", "gap-4", "justify-between", "items-center", "px-4", "py-1");
+
+        const seatNmbr = document.createElement("p");
+        const seatPrice = document.createElement("p");
+        const seatClassOutput = document.createElement("p")
+        
+        seatNmbr.innerText = seatNumber;
+        seatNmbr.classList.add("font-semibold");
+
+        seatPrice.innerText = ticketPrice;
+
+        seatClassOutput.innerText = "Economy";
+        
+        
+
+        div.appendChild(seatNmbr);
+        div.appendChild(seatClassOutput);
+        div.appendChild(seatPrice);
+
+        seatSelectedContainer.appendChild(div);
+
+
         // console.log(event.target.);
     })
 }
+
+
 
 
 
